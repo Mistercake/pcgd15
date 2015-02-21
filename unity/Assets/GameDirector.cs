5590f8095a4497e31c56ad463b15f6acfc7031aa
Nikke <nileskel@gmail.com>
2015-02-21 19:44:52 +0200

    implemented pause menu

--- unity/Assets/GameDirector.cs
+++ unity/Assets/GameDirector.cs
@@ -5,7 +5,9 @@ public class GameDirector : MonoBehaviour {
 	
 	Animator animator;
 	PlayerMovement player;
-	
+	public static bool gamePaused = false;
+	Component pauseMenu = null;
+
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
@@ -15,11 +17,24 @@ public class GameDirector : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(player.IsDead()) Invoke("SlideOut", 1f);
+		// show pause menu when esc pressed
+		if (Input.GetKeyDown ("escape")) {
+			if (!gamePaused) {
+				Time.timeScale = 0;
+				gamePaused = true;
+				pauseMenu = gameObject.AddComponent("PauseMenu");
+			} else {
+				Time.timeScale = 1;
+				gamePaused = false;
+				Destroy (pauseMenu);
+			}
+		}
 	}
 	
 	void SlideOut(){
 		animator.SetTrigger("SlideOut");
 		Invoke("NextLevel", 1.3f);
+
 	}
 	
 	void Restart(){
